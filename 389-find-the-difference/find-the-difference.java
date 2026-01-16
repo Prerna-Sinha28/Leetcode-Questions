64c654f7ffe441e8726cf0d5e2ce32a999833fb9
PRERNA SINHA <prernasinha553@gmail.com>
2026-01-16 20:01:58 +0530

    Time: 1 ms (99.73%) | Memory: 42.9 MB (94.49%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+     char ans=0;
+        for(char c:s.toCharArray())
+        ans=(char)(ans^c);
+        for(char c:t.toCharArray())
+        ans=(char)(ans^c);
+
+        return ans;
+    }
+
+}
